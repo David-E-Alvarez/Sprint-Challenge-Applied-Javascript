06d7adaac258c5eb3709f3f4d298499a7a497c3a
david <davidealvarez1@gmail.com>
2020-02-14 13:22:10 -0800

    header

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,5 +8,18 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .header-container component
-
-function Header() {}
+const header = document.querySelector('.header-container');
+header.append(Header());
+function Header() {
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const weather = document.createElement('span');
+    header.appendChild(date);
+    header.appendChild(title);
+    header.appendChild(weather);
+    header.classList.add('header');
+    date.classList.add('date');
+    weather.classList.add('temp');
+    return header;
+}
